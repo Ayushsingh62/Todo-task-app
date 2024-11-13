728ee58f7611b947c91231c4bfbfa444801f1dff
Ayushsingh62 <ayushsinghparihar48@gmail.com>
2024-11-13 13:37:35 +0530

    second commit

--- script.js
+++ script.js
@@ -3,7 +3,11 @@
  
  const form = document.querySelector('form')
 
- const input = document.querySelector('input')
+ const name = document.querySelector('#name')
+
+ const email = document.querySelector('#email')
+
+ const phone = document.querySelector('#phone')
   const ul = document.querySelector('ul')
 
   form.addEventListener('submit', Savetask)
@@ -18,13 +22,19 @@
 
 
     list = document.createElement('li')
-    console.log(input.value)
-    list.innerText=input.value
- 
-
+    console.log(name.value)
+    list.innerText=name.value
+    para = document.createElement('li')
+ para.innerText=email.value
+number=document.createElement('li')
+number.innerText=phone.value
 
     list.className = "list-group-item w-50 mt-2 "
+     para.className = "list-group-item w-50 mt-2 "
+      number.className = "list-group-item w-50 mt-2 "
     ul.appendChild(list)
+    ul.appendChild(para)
+    ul.appendChild(number)
    
     form.reset()
 
@@ -33,4 +43,29 @@
    
  delet.innerText="Delete"
   list.appendChild(delet)
+  const deletes= document.createElement('button')
+    deletes.className='btn btn-danger text-light float-end'
+    deletes.innerText="Delete"
+   
+  para.appendChild(deletes)
+  const cut = document.createElement('button')
+  cut.className='btn btn-danger text-light float-end'
+  number.appendChild(cut)
+  cut.innerText=" Delete"
   }
+  
+function DeleteTask(e){
+
+  if(e.target.className.includes("btn")){
+      li = e.target.parentElement 
+      console.log(li)
+      li.remove()
+  }
+
+
+
+}
+
+
+
+ul.addEventListener('click' , DeleteTask)
